7eea53c89993b85b05cafe95565e61ad3fb04ee0
akotu235 <akotu@protonmail.com>
2024-10-31 11:03:57 +0100

    Improved route display

--- src/main/java/io/github/akotu235/tsp/model/Route.java
+++ src/main/java/io/github/akotu235/tsp/model/Route.java
@@ -10,6 +10,6 @@ public record Route(List<Node> nodes, double totalCost, String costUnit) {
                 .map(Node::getName)
                 .collect(Collectors.joining(" -> "));
 
-        return String.format("Best result: %.0f %s\nRoute: %s\n", totalCost, costUnit, nodeNames);
+        return String.format("Total cost: %.0f %s\nRoute: %s\n", totalCost, costUnit, nodeNames);
     }
 }
